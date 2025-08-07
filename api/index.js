6d17fb8859e4304ebb20d9b51994d4f051b90c53
abhijithes6 <abhijithes266.com>
2025-08-08 01:44:47 +0530

    first

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = require('json-server');
-const app = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-app.use(middlewares);
-app.use(router);
-
-module.exports = app;
